ac58cb1dcedeef59148c037721d39f81aee32fe7
Andi Maliqi <144494359+Maliqi06@users.noreply.github.com>
2025-03-12 16:39:42 +0100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -24,16 +24,16 @@ IF OBJECT_ID('bronze.restaurant_cons', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.restaurant_cons (
-    consumer_ID NVARCHAR(50), 
+    consumer_id NVARCHAR(50), 
     city NVARCHAR(50),
     state_info NVARCHAR(50),
     country NVARCHAR(50),
     latitude DECIMAL(15, 10),
     longitude DECIMAL(15, 10),
     smoker NVARCHAR(30),
-    drink_Level NVARCHAR(50),
-    transportation_Method NVARCHAR(50),
-    marital_Status NVARCHAR(50),
+    drink_level NVARCHAR(50),
+    transportation_method NVARCHAR(50),
+    marital_status NVARCHAR(50),
     children NVARCHAR(30),
     age INT,
     occupation NVARCHAR(50),
@@ -46,16 +46,16 @@ IF OBJECT_ID('bronze.restaurant_rest', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.restaurant_rest(
-    restaurant_ID NVARCHAR(50),
+    restaurant_id NVARCHAR(50),
     name_of_restaurant NVARCHAR(100),
     city NVARCHAR(50),
     state_info NVARCHAR(50),
     country NVARCHAR(50),
-    zip_Code NVARCHAR(30),
+    zip_code NVARCHAR(30),
     latitude DECIMAL(15, 10),
     longitude DECIMAL(15, 10),
-    alcohol_Service NVARCHAR(30),  
-    smoking_Allowed NVARCHAR(30),  
+    alcohol_service NVARCHAR(30),  
+    smoking_allowed NVARCHAR(30),  
     price NVARCHAR(50),  
     franchise NVARCHAR(30),  
     area NVARCHAR(50),  
@@ -68,7 +68,7 @@ IF OBJECT_ID('bronze.restaurant_rest_cui', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.restaurant_rest_cui (
-    restaurant_ID int,
+    restaurant_id int,
     cuisine NVARCHAR(50)
 );
 GO
@@ -78,10 +78,10 @@ IF OBJECT_ID('bronze.restaurant_rat', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.restaurant_rat (
-    consumer_ID NVARCHAR(50),
-    restaurant_ID INT,
-    overall_Rating INT,
-    food_Rating INT,
-    service_Rating INT,
+    consumer_id NVARCHAR(50),
+    restaurant_id INT,
+    overall_rating INT,
+    food_rating INT,
+    service_rating INT,
 );
 GO
