15b390a19176aaa7b057239c958219f0bd8f9d6a
Andi Maliqi <144494359+Maliqi06@users.noreply.github.com>
2025-03-12 15:31:36 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,41 +1,67 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
-BULK INSERT bronze.restaurant_rest_cui
-FROM 'C:\restaurant\restaurant_cuisines.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-)
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
 
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
 
-BULK INSERT bronze.restaurant_rest
-FROM 'C:\restaurant\restaurants.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-)
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN	
+	
+	TRUNCATE TABLE bronze.restaurant_rest_cui;
+	BULK INSERT bronze.restaurant_rest_cui
+	FROM 'C:\Users\Andi\Desktop\restaurant\restaurant_cuisines.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
+	TRUNCATE TABLE bronze.restaurant_rest
+	BULK INSERT bronze.restaurant_rest
+	FROM 'C:\Users\Andi\Desktop\restaurant\restaurants.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.restaurant_rat
-FROM 'C:\restaurant\ratings.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-)
+	TRUNCATE TABLE bronze.restaurant_rat
+	BULK INSERT bronze.restaurant_rat
+	FROM 'C:\Users\Andi\Desktop\restaurant\ratings.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.restaurant_cons
-FROM 'C:\restaurant\consumers.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-)
+	TRUNCATE TABLE bronze.restaurant_cons
+	BULK INSERT bronze.restaurant_cons
+	FROM 'C:\Users\Andi\Desktop\restaurant\consumers.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.restaurant_cons_pref
-FROM 'C:\restaurant\consumer_preferences.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.restaurant_cons_pref
+	BULK INSERT bronze.restaurant_cons_pref
+	FROM 'C:\Users\Andi\Desktop\restaurant\consumer_preferences.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
+END
