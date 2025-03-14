3f22761d73b9c4fda255ae0ea7a7eeecae9b1224
Andi Maliqi <144494359+Maliqi06@users.noreply.github.com>
2025-03-14 14:30:07 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -17,8 +17,6 @@ Usage Example:
 ===============================================================================
 */
 
-EXEC bronze.load_bronze;
-
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN	
 	
